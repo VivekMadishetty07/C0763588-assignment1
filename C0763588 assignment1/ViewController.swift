508b68a47f5398349fa306ce1185051eee2561d2
Vivek <VivekMadishetty07>
2020-01-14 16:00:03 -0500

    Initial Commit

---
+++ C0763588 assignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0763588 assignment1
+//
+//  Created by Vivek Madishetty on 2020-01-14.
+//  Copyright © 2020 Vivek Madishetty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
